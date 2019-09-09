d4a8145efdf3a40a19c6d9e07036ef1f24e73054
Harlan Heath <harlandheath@gmail.com>
2019-09-08 19:43:31 -0500

    created components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React, { Component } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import NavBar from "./components/Navbar";
+import Cart from "./components/Cart";
+import Default from "./components/Default";
+import Details from "./components/Details";
+import ProductList from "./components/ProductList";
 
 export default class App extends Component {
   render() {
-    return <React.Fragment></React.Fragment>;
+    return (
+      <React.Fragment>
+        <NavBar></NavBar>
+        <ProductList></ProductList>
+        <Details></Details>
+        <Cart></Cart>
+        <Default></Default>
+      </React.Fragment>
+    );
   }
 }
