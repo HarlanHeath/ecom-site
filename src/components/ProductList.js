d7d6cf9bec5f4732b40e28a655583721c7b21c77
Harlan Heath <harlandheath@gmail.com>
2019-09-10 20:14:45 -0500

    setting up Context

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,19 +1,29 @@
 import React, { Component } from "react";
 import Product from "./Product";
 import Title from "./Title";
+import { storeProducts } from "../data";
+import { ProductConsumer } from "../context";
 
 export default class ProductList extends Component {
   state = {
-    products: []
+    products: storeProducts
   };
 
   render() {
+    console.log(this.state.products);
     return (
       <React.Fragment>
         <div className="py-5">
           <div className="container">
             <Title name="our" title="products" />
-            <div className="row"></div>
+
+            <div className="row">
+              <ProductConsumer>
+                {hello => {
+                  return <h1>{hello}</h1>;
+                }}
+              </ProductConsumer>
+            </div>
           </div>
         </div>
       </React.Fragment>
